2f8ed5a48b0922a842c5c907be301a66e0ef77d2
Ricardo ospina <ricardorlo@hotmail.es>
2020-01-17 13:55:13 -0500

    ...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+var axios = require('axios');
+var MockAdapter = require('axios-mock-adapter');
+ 
+// This sets the mock adapter on the default instance
+var mock = new MockAdapter(axios);
+ 
+// Mock any GET request to /users
+// arguments for reply are (status, data, headers)
+mock.onGet('/users').reply(200, {
+  users: [
+    { id: 1, name: 'John Smith' ,valor:22223}
+  ]
+});
+ 
+axios.get('/users')
+  .then(function(response) {
+    console.log(response.data);
+  });
 function App() {
   return (
     <div className="App">
